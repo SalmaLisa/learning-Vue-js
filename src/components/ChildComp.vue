7c343e02d4a1312ffdb14e7e368a1c7de32cb44b
Salmalisa <salmalisa7342@gmail.com>
2023-02-10 11:51:16 +0600

    first commit

---
+++ src/components/ChildComp.vue
@@ -0,0 +1,8 @@
+<template>
+  <h2>This is child component</h2>
+</template>
+<script>
+export default {
+  name:"ChildComp"
+}
+</script>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+<h1>Don't be afraid, Salma. Your prime guardian is always here for you. keep faith on Him and follow the route He guides.</h1>
+<p v-if="show">Show</p>
+<p v-else>Hide</p>
+<button @click="show=!show">toggle</button>
+<ChildComp></ChildComp>
+</template>
+
+<script>
+import ChildComp from './ChildComp.vue';
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      tech:["react","vue","angular"]
+    }
+    
+  },
+      methods: {
+        getData() {
+          console.log()
+     }
+  },
+  components: {
+      ChildComp
+    }
+}
+</script>
+<style scoped>
+h1{
+  font-size: 50px;
+  color: blue;
+}
+</style>
